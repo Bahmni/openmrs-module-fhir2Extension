70a6f98596f696b602072a73e5668181860400c1
manimaarans <manimaaran@beehyv.com>
2023-07-27 11:02:49 +0530

    BS-151| Vijay, Siva, Mani | removed birthdate and deceasedDateTime as they are not part of story

--- api/src/main/java/org/openmrs/module/fhirExtension/export/anonymise/impl/AddressResourceRedact.java
+++ api/src/main/java/org/openmrs/module/fhirExtension/export/anonymise/impl/AddressResourceRedact.java
@@ -5,22 +5,23 @@ import org.hl7.fhir.r4.model.Patient;
 import org.openmrs.module.fhirExtension.export.anonymise.ResourceRedact;
 
 public class AddressResourceRedact implements ResourceRedact {
-    private AddressResourceRedact() {
-
-    }
-
-    @Override
-    public void redact(IBaseResource iBaseResource) {
-        Patient patient = (Patient) iBaseResource;
-        patient.setAddress(null);
-    }
-
-    private static class SingletonHelper {
-
-        private static final AddressResourceRedact INSTANCE = new AddressResourceRedact();
-    }
-
-    public static AddressResourceRedact getInstance() {
-        return AddressResourceRedact.SingletonHelper.INSTANCE;
-    }
+	
+	private AddressResourceRedact() {
+		
+	}
+	
+	@Override
+	public void redact(IBaseResource iBaseResource) {
+		Patient patient = (Patient) iBaseResource;
+		patient.setAddress(null);
+	}
+	
+	private static class SingletonHelper {
+		
+		private static final AddressResourceRedact INSTANCE = new AddressResourceRedact();
+	}
+	
+	public static AddressResourceRedact getInstance() {
+		return AddressResourceRedact.SingletonHelper.INSTANCE;
+	}
 }
--- api/src/main/java/org/openmrs/module/fhirExtension/export/anonymise/impl/TelecomResourceRedact.java
+++ api/src/main/java/org/openmrs/module/fhirExtension/export/anonymise/impl/TelecomResourceRedact.java
@@ -4,24 +4,24 @@ import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Patient;
 import org.openmrs.module.fhirExtension.export.anonymise.ResourceRedact;
 
-
 public class TelecomResourceRedact implements ResourceRedact {
-    private TelecomResourceRedact() {
-
-    }
-
-    @Override
-    public void redact(IBaseResource iBaseResource) {
-        Patient patient = (Patient) iBaseResource;
-        patient.setTelecom(null);
-    }
-
-    private static class SingletonHelper {
-
-        private static final TelecomResourceRedact INSTANCE = new TelecomResourceRedact();
-    }
-
-    public static TelecomResourceRedact getInstance() {
-        return TelecomResourceRedact.SingletonHelper.INSTANCE;
-    }
+	
+	private TelecomResourceRedact() {
+		
+	}
+	
+	@Override
+	public void redact(IBaseResource iBaseResource) {
+		Patient patient = (Patient) iBaseResource;
+		patient.setTelecom(null);
+	}
+	
+	private static class SingletonHelper {
+		
+		private static final TelecomResourceRedact INSTANCE = new TelecomResourceRedact();
+	}
+	
+	public static TelecomResourceRedact getInstance() {
+		return TelecomResourceRedact.SingletonHelper.INSTANCE;
+	}
 }
