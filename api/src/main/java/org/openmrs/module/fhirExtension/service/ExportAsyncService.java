eb734d1d329b033c4d6c8a1c7288370470f6ece9
vijayanandtwks <113415613+vijayanandtwks@users.noreply.github.com>
2023-11-13 09:10:03 +0530

    BS-291 | Vijay | add User Name, Start Date and End Date as part of initialize (#46)
    * BS-291 | Vijay | add User Name, Start Date and End Date as part of initialize
    
    * BS-291 | Vijay | resolved bean name

--- api/src/main/java/org/openmrs/module/fhirExtension/service/ExportAsyncService.java
+++ api/src/main/java/org/openmrs/module/fhirExtension/service/ExportAsyncService.java
@@ -2,14 +2,11 @@ package org.openmrs.module.fhirExtension.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.openmrs.Concept;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.context.UserContext;
 import org.openmrs.module.fhir2.api.dao.FhirTaskDao;
 import org.openmrs.module.fhir2.model.FhirTask;
-import org.openmrs.module.fhir2.model.FhirTaskInput;
-import org.openmrs.module.fhir2.model.FhirTaskOutput;
 import org.openmrs.module.fhirExtension.export.Exporter;
 import org.openmrs.module.fhirExtension.export.anonymise.handler.AnonymiseHandler;
 import org.openmrs.module.fhirExtension.export.anonymise.impl.CorrelationCache;
@@ -24,16 +21,6 @@ import java.util.*;
 @Component
 @Transactional
 public class ExportAsyncService {
-	
-	public static final String DOWNLOAD_URL = "Download URL";
-	
-	public static final String USER_NAME_CONCEPT = "FHIR Export User Name";
-	
-	public static final String START_DATE_CONCEPT = "FHIR Export Start Date";
-	
-	public static final String END_DATE_CONCEPT = "FHIR Export End Date";
-	
-	public static final String ANONYMISE_CONCEPT = "FHIR Export Anonymise Flag";
 	
 	private FhirTaskDao fhirTaskDao;
 	
@@ -56,8 +43,7 @@ public class ExportAsyncService {
 	}
 	
 	@Async("export-fhir-data-threadPoolTaskExecutor")
-	public void export(FhirTask fhirTask, String startDate, String endDate, UserContext userContext, String downloadUrl,
-	        boolean isAnonymise) {
+	public void export(FhirTask fhirTask, String startDate, String endDate, UserContext userContext, boolean isAnonymise) {
 		FhirTask.TaskStatus taskStatus = null;
 		
 		try {
@@ -73,10 +59,6 @@ public class ExportAsyncService {
 			}
 			
 			fileExportService.createZipWithExportedNdjsonFiles(fhirTask.getUuid());
-			FhirTaskOutput fhirTaskOutput = getFhirTaskOutput(fhirTask, downloadUrl);
-			fhirTask.setOutput(Collections.singleton(fhirTaskOutput));
-			Set<FhirTaskInput> fhirTaskInputs = getFhirTaskInputs(fhirTask, startDate, endDate, isAnonymise);
-			fhirTask.setInput(fhirTaskInputs);
 			fileExportService.deleteDirectory(fhirTask.getUuid());
 		}
 		catch (Exception exception) {
@@ -96,36 +78,6 @@ public class ExportAsyncService {
 		fileExportService.createDirectory(fhirTask.getUuid());
 		correlationCache.reset();
 		anonymiseHandler.loadAnonymiserConfig(isAnonymise);
-	}
-	
-	private Set<FhirTaskInput> getFhirTaskInputs(FhirTask fhirTask, String startDate, String endDate, boolean isAnonymise) {
-		FhirTaskInput userNameFhirTaskInput = createFHIRTaskInput(fhirTask, USER_NAME_CONCEPT,	Context.getAuthenticatedUser().getUsername());
-        FhirTaskInput startDateFhirTaskInput = createFHIRTaskInput(fhirTask, START_DATE_CONCEPT, startDate);
-        FhirTaskInput endDateFhirTaskInput = createFHIRTaskInput(fhirTask, END_DATE_CONCEPT, endDate);
-        FhirTaskInput anonymiseFhirTaskInput = createFHIRTaskInput(fhirTask, ANONYMISE_CONCEPT, Boolean.toString(isAnonymise));
-        return new HashSet<>(Arrays.asList(userNameFhirTaskInput, startDateFhirTaskInput, endDateFhirTaskInput, anonymiseFhirTaskInput));
-    }
-	
-	private FhirTaskInput createFHIRTaskInput(FhirTask fhirTask, String conceptName, String valueText) {
-		Concept concept = conceptService.getConceptByName(conceptName);
-		if (concept == null) {
-			throw new RuntimeException("Concept with name " + conceptName + " not found");
-		}
-		FhirTaskInput fhirTaskInput = new FhirTaskInput();
-		fhirTaskInput.setName(conceptName);
-		fhirTaskInput.setTask(fhirTask);
-		fhirTaskInput.setValueText(valueText);
-		fhirTaskInput.setType(concept);
-		return fhirTaskInput;
-	}
-	
-	private FhirTaskOutput getFhirTaskOutput(FhirTask fhirTask, String downloadUrl) {
-		FhirTaskOutput fhirTaskOutput = new FhirTaskOutput();
-		fhirTaskOutput.setName("Download Link Name");
-		fhirTaskOutput.setTask(fhirTask);
-		fhirTaskOutput.setValueText(downloadUrl + "?file=" + fhirTask.getUuid());
-		fhirTaskOutput.setType(conceptService.getConceptByName(DOWNLOAD_URL));
-		return fhirTaskOutput;
 	}
 	
 	private void anonymise(List<IBaseResource> iBaseResources, String resourceType, boolean isAnonymise) {
--- api/src/test/java/org/openmrs/module/fhirExtension/service/impl/ExportTaskTest.java
+++ api/src/test/java/org/openmrs/module/fhirExtension/service/impl/ExportTaskTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.openmrs.Concept;
 import org.openmrs.Person;
 import org.openmrs.User;
+import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.fhir2.api.dao.FhirTaskDao;
 import org.openmrs.module.fhir2.model.FhirTask;
@@ -19,12 +21,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Context.class)
 @PowerMockIgnore("javax.management.*")
 public class ExportTaskTest {
+	
+	@Mock
+	private ConceptService conceptService;
 	
 	@Mock
 	private FhirTaskDao fhirTaskDao;
@@ -43,10 +50,17 @@ public class ExportTaskTest {
 	@Test
 	public void shouldCreateFhirTask_whenRequestedForPatientDataExport() {
 		when(fhirTaskDao.createOrUpdate(any(FhirTask.class))).thenReturn(mockFhirTask());
-		
-		FhirTask initialTaskResponse = exportTask.getInitialTaskResponse(false);
+		when(conceptService.getConceptByName(any())).thenReturn(new Concept());
+		FhirTask initialTaskResponse = exportTask.getInitialTaskResponse("2023-05-01", "2023-05-31", "http://dummyUrl",
+		    false);
 		
 		assertNotNull(initialTaskResponse);
+		verify(conceptService, times(1)).getConceptByName("Download URL");
+		verify(conceptService, times(1)).getConceptByName("FHIR Export User Name");
+		verify(conceptService, times(1)).getConceptByName("FHIR Export Start Date");
+		verify(conceptService, times(1)).getConceptByName("FHIR Export End Date");
+		verify(conceptService, times(1)).getConceptByName("FHIR Export Anonymise Flag");
+		verify(fhirTaskDao, times(1)).createOrUpdate(any(FhirTask.class));
 		assertEquals(FhirTask.TaskStatus.ACCEPTED, initialTaskResponse.getStatus());
 	}
 	
